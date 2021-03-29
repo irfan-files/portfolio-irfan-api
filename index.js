bee1cc71cbb4f02533d37eb00759192f41acd398
irfan <irfan.maulana@students.amikom.ac.id>
2021-03-30 05:08:34 +0700

    mongo db connect

--- index.js
+++ index.js
@@ -1,9 +1,31 @@
 const express = require("express");
 const server = express();
+const portfolioRoutes = require("./routes/portfolios");
+const config = require("./config/dev");
+const mongoose = require("mongoose");
+
+mongoose.connect(
+  config.DB_URI,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  },
+  (err) => {
+    if (err) {
+      console.error(err);
+    } else {
+      console.log("connected to DB");
+    }
+  }
+);
 
 server.get("/test", (req, res) => {
   return res.json({ message: "test is working" });
 });
+
+//get end point /api/v1/portfolio
+server.use("/api/v1/portfolios", portfolioRoutes);
 
 const PORT = parseInt(process.env.PORT, 10) || 3001;
 server.listen(PORT, (err) => {
---
+++ routes/portfolios.js
@@ -0,0 +1,6 @@
+const express = require("express");
+const { getPortfolios } = require("../controllers/portfolios");
+const router = express.Router();
+
+router.get("", getPortfolios);
+module.exports = router;
