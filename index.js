3014e5a592ed9bf951b9d0705c4b018d2c1ce5ed
irfan <irfan.maulana@students.amikom.ac.id>
2021-03-31 18:20:03 +0700

    u

--- index.js
+++ index.js
@@ -1,34 +1,22 @@
 const express = require("express");
 const server = express();
 const portfolioRoutes = require("./routes/portfolios");
-const config = require("./config/dev");
-const mongoose = require("mongoose");
 
-mongoose.connect(
-  config.DB_URI,
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-  },
-  (err) => {
-    if (err) {
-      console.error(err);
-    } else {
-      console.log("connected to DB");
-    }
-  }
-);
+async function runServer() {
+  await require("./db").connect();
 
-server.get("/test", (req, res) => {
-  return res.json({ message: "test is working" });
-});
+  server.get("/test", (req, res) => {
+    return res.json({ message: "test is working" });
+  });
 
-//get end point /api/v1/portfolio
-server.use("/api/v1/portfolios", portfolioRoutes);
+  //get end point /api/v1/portfolio
+  server.use("/api/v1/portfolios", portfolioRoutes);
 
-const PORT = parseInt(process.env.PORT, 10) || 3001;
-server.listen(PORT, (err) => {
-  if (err) console.error(err);
-  console.log("server ready on port:", PORT);
-});
+  const PORT = parseInt(process.env.PORT, 10) || 3001;
+  server.listen(PORT, (err) => {
+    if (err) console.error(err);
+    console.log("server ready on port:", PORT);
+  });
+}
+
+runServer();
